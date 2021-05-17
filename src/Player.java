fdc539d958026e48cc9906d66e0127700244e5e0
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 15:48:40 +0200

    add Player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,26 @@
 public class Player {
 
+    String name;
+    char mark;
 
+    Player(String name, char mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getMark() {
+        return mark;
+    }
+
+    public void setMark(char mark) {
+        this.mark = mark;
+    }
 }
