f39b2224322733b0ca66f60710589ef888cfd68d
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 17:02:36 +0200

    add commentator to print players actions

---
+++ src/main/java/Commentator.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Commentator implements Printable{
+
+    ArrayList<Player> players;
+
+    Commentator(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    @Override
+    public void print(ArrayList<Move> moves) {
+
+        Move move = moves.get(moves.size() - 1);    // get last move
+        Player p = players.get(move.playerNum);
+
+        StringBuilder sb = new StringBuilder("Player ")
+                .append(p.getName())
+                .append(" put ")
+                .append(p.getMark())
+                .append(" at [")
+                .append(move.x)
+                .append("][")
+                .append(move.y)
+                .append("]");
+
+        System.out.println(sb.toString());
+    }
+}
