4e5e309ad7cbc980c1b0063934bbfb9770043a67
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 15:53:38 +0200

    add players to the game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    Player playerOne;
+    Player playerTwo;
+
+    public TicTacToe(Player one, Player two) {
+        playerOne = one;
+        playerTwo = two;
+    }
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter player 1. (O mark) name: ");
+        Player playerOne = new Player(scan.nextLine(), 'O');
+        System.out.println("Enter player 2. (X mark) name: ");
+        Player playerTwo = new Player(scan.nextLine(), 'X');
+
+        TicTacToe game = new TicTacToe(playerOne, playerTwo);
+
+        
     }
 }
