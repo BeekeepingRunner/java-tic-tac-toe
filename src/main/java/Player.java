450bae2cb59da35cb7485303b9323d65ea428495
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 17:23:15 +0200

    add player id

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,15 @@
 public class Player {
 
     String name;
+    int id;
     char mark;
+
+    private static int nextId = 0;
 
     Player(String name, char mark) {
         this.name = name;
         this.mark = mark;
+        id = ++nextId;
     }
 
     public String getName() {
@@ -22,5 +26,9 @@ public class Player {
 
     public void setMark(char mark) {
         this.mark = mark;
+    }
+
+    public int getId() {
+        return this.id;
     }
 }
