e4cdd78f14829dc89447c1d32404c2ae72ff5c94
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 16:31:39 +0200

    add board printing

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Board {
+public class Board implements Printable {
 
     public static final int gridDimension = 3;
 
@@ -14,6 +14,32 @@ public class Board {
             {
                 fields[i][j] = '\f';
             }
+        }
+    }
+
+    @Override
+    public void print(ArrayList<Move> moves) {
+
+        for (Move move : moves)
+        {
+            switch (move.playerNum)
+            {
+                case 0:
+                    fields[move.x][move.y] = 'O';
+                case 1:
+                    fields[move.x][move.y] = 'X';
+            }
+        }
+
+        for (int i = 0; i < gridDimension; ++i)
+        {
+            // e.g. print X|O|X
+            System.out.print(fields[i][gridDimension - 3] + "|");
+            System.out.print(fields[i][gridDimension - 2] + "|");
+            System.out.println(fields[i][gridDimension - 1]);
+
+            if (i < 2)
+                System.out.println("\n------");
         }
     }
 }
