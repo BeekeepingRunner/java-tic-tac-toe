8df4411be48dd6331d2a7493e3d19dbd45eaf184
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-17 17:24:31 +0200

    add commentator test

--- src/test/java/CommentatorTest.java
+++ src/test/java/CommentatorTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CommentatorTest {
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+public class CommentatorTest {
+
+    private ArrayList<Move> moves;
+    private ArrayList<Player> players;
+    private Commentator commentator;
+
+    @Before
+    void setUp() {
+        players = new ArrayList<>();
+        players.add(new Player("P1", 'O'));
+        players.add(new Player("P2", 'X'));
+        commentator = new Commentator(players);
+    }
 }
